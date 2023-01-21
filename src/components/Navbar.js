d89d553b2500866000e1792db3a8b761eebec0ea
briantkho <briantkho@gmail.com>
2023-01-21 14:06:15 -0800

    navbar init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Logo from "../assets/logo.svg"
+import { ProfileIcon } from './ProfileIcon'
 
 export const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className="flex place-content-between">
+        <img className='w-12 h-auto' src={Logo} alt="Logo" />
+        <div className='flex gap-8'>
+            <p>Reports</p>
+            <p>Message</p>
+            <ProfileIcon />
+        </div>
+    </div>
   )
 }
---
+++ src/components/ProfileIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ProfileIcon = () => {
+  return (
+    <div>
+        <div className='bg-primary-500'>
+            <p>JD</p>
+        </div>
+    </div>
+  )
+}
